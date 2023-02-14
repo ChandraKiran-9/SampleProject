bbd77c3f23ef8b740b4fe31ef6a29c262ac1e582
ChandraKiran-9 <chandrakiranjck9@gmail.com>
2023-02-14 10:46:16 +0530

    First commit

---
+++ Demo.java
@@ -0,0 +1,11 @@
+package Java_Intro;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Eclipse IDE !!");
+		System.out.println("Git Intro");
+
+	}
+}
---
+++ Intro.java
@@ -0,0 +1,10 @@
+package Java_Intro;
+
+public class Intro {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Eclipse IDE !!");
+
+	}
+}
